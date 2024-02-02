b3beee186c36cf1fc57cf0f4cf2788765debc43a
P.Gangothri <pgangothriroyal@gmail.com>
2024-02-02 19:01:31 +0530

    Initial commit

---
+++ loanproject/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path ,include
+from customerapp import views
+from loanapp import urls as loanapp_urls
+
+#from loanapp import views
+
+
+urlpatterns = [
+    
+   path('register/',views.register_customer, name='register_customer'),
+  path('loan/',include(loanapp_urls)),
+]
